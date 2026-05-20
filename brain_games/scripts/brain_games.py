402f7c538d28c3ba15bfec334e6565fb216eddfa
AleksVedenyev <aleks.vedenev@gmail.com>
2026-05-20 15:26:08 +0300

    add cli module with greeting question

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
+from brain_games.cli import welcome_user
+
 def main():
     def greet():
         print('Welcome to the Brain Games!')
     greet()
+    welcome_user()
 
 
 if __name__ == "__main__":
